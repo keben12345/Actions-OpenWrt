0bfefd0511edc6b2cfee113a920ed8a55e39410c
keben12345 <79047578+keben12345@users.noreply.github.com>
2022-12-16 06:58:28 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -14,9 +14,9 @@
 sed -i 's/192.168.1.1/192.168.1.88/g' package/base-files/files/bin/config_generate
 rm -rf package/trojan-plus
 git clone https://github.com/Trojan-Plus-Group/trojan-plus.git package/trojan-plus
-rm -rf package/helloworld
-git clone --depth=1 https://github.com/fw876/helloworld.git package/helloworld
-# rm -rf package/passwall
-# git clone https://github.com/xiaorouji/openwrt-passwall package/passwall
+#rm -rf package/helloworld
+#git clone --depth=1 https://github.com/fw876/helloworld.git package/helloworld
+rm -rf package/passwall
+git clone https://github.com/xiaorouji/openwrt-passwall package/passwall
 rm -rf package/bypass
 git clone https://github.com/kiddin9/openwrt-bypass.git package/bypass

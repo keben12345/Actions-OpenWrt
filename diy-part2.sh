370be015f580c0da389ed92e9df40dcd689692c5
keben12345 <79047578+keben12345@users.noreply.github.com>
2022-12-16 11:09:35 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,7 +11,7 @@
 #
 
 # Modify default IP
-sed -i 's/192.168.1.1/192.168.1.88/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.1.1/g' package/base-files/files/bin/config_generate
 rm -rf package/trojan-plus
 git clone https://github.com/Trojan-Plus-Group/trojan-plus.git package/trojan-plus
 #rm -rf package/helloworld

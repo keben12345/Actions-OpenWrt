b9344c015ef3908418f45a1934a5914d4e43b812
keben12345 <79047578+keben12345@users.noreply.github.com>
2025-02-23 21:16:04 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -14,12 +14,17 @@
 sed -i 's/192.168.1.1/192.168.1.1/g' package/base-files/files/bin/config_generate
 #rm -rf package/trojan-plus
 #git clone https://github.com/Trojan-Plus-Group/trojan-plus.git package/trojan-plus
+git clone -b master --single-branch https://github.com/immortalwrt/packages.git packages_master
+rm -rf ./feeds/packages/lang/golang
+mv ./packages_master/lang/golang ./feeds/packages/lang
+rm -rf ./packages_master
 rm -rf package/helloworld
 git clone --depth=1 https://github.com/fw876/helloworld.git package/helloworld
-rm -rf package/passwall_package
-git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall-packages.git package/passwall_package
-rm -rf package/passwall
-git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall.git package/passwall
+rm -rf feeds/luci/applications/luci-app-passwall/
+rm -rf feeds/packages/net/xray-core/
+rm -rf feeds/packages/net/xray-plugin/
+git clone https://github.com/xiaorouji/openwrt-passwall package/luci-app-passwall
+git clone https://github.com/xiaorouji/openwrt-passwall-packages package/luci-app-passwall-packages
 #rm -rf package/bypass
 #git clone https://github.com/kiddin9/openwrt-bypass.git package/bypass
 rm -rf lua-maxminddb

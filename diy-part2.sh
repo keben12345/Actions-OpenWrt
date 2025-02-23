2031ac839661b8d82d1671cce73809235420e911
keben12345 <79047578+keben12345@users.noreply.github.com>
2025-02-23 17:08:32 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -16,8 +16,9 @@ sed -i 's/192.168.1.1/192.168.1.1/g' package/base-files/files/bin/config_generat
 #git clone https://github.com/Trojan-Plus-Group/trojan-plus.git package/trojan-plus
 rm -rf package/helloworld
 git clone --depth=1 https://github.com/fw876/helloworld.git package/helloworld
-#rm -rf package/passwall
-#git clone https://github.com/xiaorouji/openwrt-passwall package/passwall
+rm -rf package/passwall
+git clone https://github.com/xiaorouji/openwrt-passwall package/passwall
+# git clone https://github.com/xiaorouji/openwrt-passwall-packages
 #rm -rf package/bypass
 #git clone https://github.com/kiddin9/openwrt-bypass.git package/bypass
 rm -rf lua-maxminddb
